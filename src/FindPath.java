a75a417ee8f5f00a0e2d2ec3c04778f0ff1579a7
DennisTalmid <d.loska@hotmail.de>
2017-06-20 15:49:15 +0200

    link

--- src/FindPath.java
+++ src/FindPath.java
@@ -1,5 +1,8 @@
 public class FindPath {
 
+    /*
+        https://www.youtube.com/watch?v=gdmfOwyQlcI Gutes Video zum Dijkstra Algorithmus (Greedy Version)
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
